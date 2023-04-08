7b34c182066f4e58778b61206129703bd762b0ec
Sergio Luciano Pérez <slperez@proveedor.teco.com.ar>
2023-04-08 20:22:56 -0300

    se agregan las card de noedades de forma dinamica

---
+++ js/app.js
@@ -0,0 +1,25 @@
+ const pintarCards = () => {
+     const contenedor = document.querySelector('#novedades-contenedor')
+
+     novedades.forEach( novedad => {
+        const div = document.createElement('div')
+        div.className = 'card mb-3'
+        div.innerHTML = `
+                <div class="row g-0">
+                    <div class="col-md-8">
+                        <div class="card-body">
+                        <h2 class="card-title">${novedad.titulo}</h2>
+                        <p class="card-text"><small class="text-muted">${novedad.fecha}</small></p>
+                        </div>
+                </div>
+                    <div class="col-md-4">
+                        <img src="${novedad.img}" class="img-fluid rounded-start" alt="${novedad.alt}">
+                    </div>
+                </div>
+        `
+
+        contenedor.appendChild(div)
+     })
+ };
+
+ pintarCards()
---
+++ js/novedades.js
@@ -0,0 +1,26 @@
+const novedades = [
+    {
+        titulo: "Mejoras en el sector de encuentro de nuestros socios más jóvenes",
+        fecha: "15.10.2022",
+        img: "../img/punto_joven.webp",
+        alt: "Cabaña en construccion"
+    },
+    {
+        titulo: "Instalación Cerco Perimetral Eléctrico",
+        fecha: "26.06.2022",
+        img: "../img/cerco.webp",
+        alt: "Hombre instalando un sistema de seguridad."
+    },
+    {
+        titulo: "Ampliación y mejoras en la red de suministro de agua",
+        fecha: "31.07.2021",
+        img: "../img/red_agua.webp",
+        alt: "Zanjas cavadas en el cesped para colocacion de red de agua"
+    },
+    {
+        titulo: "Ampliación sector verde de pileta",
+        fecha: "18.07.2021",
+        img: "../img/sector_verde_pileta.webp",
+        alt: "Asientos y mesas al rededor de la pileta."
+    }
+]
