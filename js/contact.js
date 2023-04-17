bcab22858d7f21730798f79bedec6da0e4c84e1f
Sergio Luciano Pérez <sluciano.perez@gamil.com>
2023-04-17 14:42:49 -0300

    se agrega verificacion para que no se pinten varios mensajes juntos en el dom y se agrega funcion para guardar y recuperar solicitudes de contacto en storage

--- js/contact.js
+++ js/contact.js
@@ -9,12 +9,29 @@ formulario.addEventListener('submit', (e) => {
     e.preventDefault()
 
     pintarMensaje(nombre.value)
+
+    guardarSolicitud(nombre.value,telefono.value,email.value,mensaje.value)
+
 })
 
+// si ya existe un div creado anteriormente, lo elimmino del dom
+function existeMensaje(container) {
+    const miDiv = document.querySelector("#containerMensaje #miDiv")
+    if (miDiv) {
+      container.removeChild(miDiv)
+    }
+  }
+
+  //pintaMensaje en el dom
 const pintarMensaje = (nombre) => {
     const container = document.querySelector('#containerMensaje')
+
+    
+    existeMensaje(container)
+
     const div = document.createElement('div')
         div.className = 'card w-100 h-100'
+        div.id = 'miDiv'
         div.innerHTML = `
                 <div class="card-body">
                     <p class="mx-3 card-text">Hola ${nombre}. Gracias por tu mensaje. Responderemos a la brevedad. Si queres comunicarte directamente con la administracion, hace click en el siguiente boton.</p>
@@ -24,4 +41,30 @@ const pintarMensaje = (nombre) => {
                 </div>
         `
     container.appendChild(div)
+
 };
+
+const contactos = []
+
+// Instancia un objeto Contacto, lo pushea en un array y lo guarda en el localStorage
+const guardarSolicitud = (nombre,telefono,email,mensaje) => {
+    const contacto = new Contacto(nombre,telefono,email,mensaje)
+
+    contactos.push(contacto);
+
+    let contactosJson = JSON.stringify(contactos);
+
+    localStorage.setItem('contactos', contactosJson)
+
+
+};
+
+// recuperar las solicitudes de localStorage y retorna un array de objetos de tipo Contacto
+const recuperarSolicitud = () => {
+    const contactosJson = localStorage.getItem('contactos')
+
+    const contactosParse = JSON.parse(contactosJson)
+
+    return contactosParse
+};
+
---
+++ js/contacto.js
@@ -0,0 +1,9 @@
+class Contacto {
+    constructor(nombre, email, telefono, mensaje) {
+        this.nombre = nombre,
+        this.email = email,
+        this.telefono = telefono,
+        this.mensaje = mensaje
+    }
+}
+
