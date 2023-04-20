081e17adc924fa50fd25d42fa7bd81ca871601b9
Sergio Luciano Pérez <sluciano.perez@gamil.com>
2023-04-20 13:23:49 -0300

    se agrega logica al form de registro para guardar en localStorage

--- js/login.js
+++ js/login.js
@@ -1,2 +1,47 @@
-const email = document.getElementById('email');
-const pass = document.getElementById('password');
+const formulario = document.getElementById('registro');
+
+const formLogin = document.getElementById('login');
+
+let usuarios = []
+
+// recupero usuarios de localStorage cada vez que se reinicia el script para mantener mi array actualizado
+recuperoUsuariosDeLocal();
+
+
+const registroUsuario = () => {
+
+    const nombre = document.getElementById('name').value;
+    const apellido = document.getElementById('lastname').value;
+    const email = document.getElementById('emailRegistro').value;
+    const pass = document.getElementById('passwordRegistro').value;
+
+    const usuario = new Usuario(nombre, apellido, email, pass)
+
+    usuarios.push(usuario)
+
+    let usuariosJson = JSON.stringify(usuarios)
+
+    localStorage.setItem('usuarios', usuariosJson);
+};
+
+formulario.addEventListener('submit',(e) => {
+    e.preventDefault()
+
+    registroUsuario()
+})
+
+formLogin.addEventListener('submit',(e) => {
+    e.preventDefault()
+
+})
+
+function recuperoUsuariosDeLocal() {
+    if(localStorage.length > 0){
+    let usuariosRec = localStorage.getItem('usuarios');
+
+    usuariosRec = JSON.parse(usuariosRec);
+    for (const usuario of usuariosRec) {
+        usuarios.push(usuario)
+    }
+    }
+}
